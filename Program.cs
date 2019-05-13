e2e2558536df1779e1a033c3be7f4c50b06beb27
Sebastian <>
2019-05-13 17:51:06 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//-------------------------------
+//-----------Delegados-----------
+//-------------------------------
+namespace Dia7Delegados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Proyecto pro = new Proyecto();
+            
+            //Console.ReadKey();
+            //pro.AgregarProyecto();
+            //pro.AgregarProyecto2();
+            //pro.Imprimir();
+
+            //Delegados (1.1)
+
+            //Func<double, double, double> raiz = CalcularRaiz;
+            //double pr=raiz(8, 2);
+            //Console.WriteLine($"Raiz de {pr}");
+            //Func<double, double, double> potencia = (x, y) => { return Math.Pow(x, y); };
+            //Console.WriteLine($"Raiz con landa {potencia(8,2)}");
+            //Func<int, int> doble = x => { return x *= 2; };
+            //Console.WriteLine($"Multiplicación con landa {doble(8)}");
+
+            var proyect = pro.AgregarProyecto2();
+            //foreach (var item in ejm1)
+            //{
+            //    Console.WriteLine($"lista{item.Nombre}");
+            //}
+            var proys = proyect.Where(x => x.Codigo>0).ToList();
+            //Console.WriteLine($"lista{proys}");
+            Console.WriteLine($"-----------------------------------" + "\n" + "-------------Ejercio 1-------------");
+            var ejem1 = proys.Where(x => x.Duracion > 6 && x.Duracion < 12).ToList();
+            //Console.WriteLine($"lista");
+            //foreach (var item in ejem1)
+            //{
+            //    Console.WriteLine($"Nombre: {item.Nombre}");
+            //    Console.WriteLine($"Codigo: {item.Codigo}");
+            //    Console.WriteLine($"Area: {item.Area}");
+            //    Console.WriteLine($"Duracion: {item.Duracion}");
+            //}
+            pro.Imprimir2(ejem1);
+
+
+            Console.ReadKey();
+            Console.WriteLine($"-----------------------------------"+"\n"+"-------------Ejercio 2-------------");
+            var ejem2 = from p in proys
+                        where (p.Nombre.StartsWith("B"))
+                        select p;
+            
+            pro.Imprimir2(ejem2.ToList());
+            Console.ReadKey();
+            Console.WriteLine($"-----------------------------------"+"\n"+"-------------Ejercio 3-------------");
+            var ejem3 = proys.Where(x => x.Codigo %7==0).ToList();
+            pro.Imprimir2(ejem3);
+            Console.ReadKey();
+            Console.WriteLine($"-----------------------------------" + "\n" + "-------------Ejercio 4-------------");
+            var ejem4 =proys.GroupBy(x =>
+              {
+                  if (x.Duracion <=6)
+                  {
+                      return "Duracion inferior a 6";
+                  }
+                  else if (x.Duracion > 6 && x.Duracion < 10)
+                  {
+                      return "Duracion inferior a 10 y mayor a 6";
+                  }
+                  
+                  else
+                  {
+                      return "Duracion mayor a 10";
+                      
+                  }
+              });
+            foreach (var groupProys in ejem4)
+            {
+                Console.ReadLine();
+                Console.WriteLine("Grupo de proyectos"+groupProys.Key+"\n");
+                
+                foreach (var item in groupProys)
+                {
+                    Console.WriteLine($"Nombre: {item.Nombre}");
+                    Console.WriteLine($"Codigo: {item.Codigo}");
+                    Console.WriteLine($"Area: {item.Area}");
+                    Console.WriteLine($"Duracion: {item.Duracion}");
+                }
+            }
+            Console.ReadKey();
+            Console.WriteLine($"-----------------------------------" + "\n" + "-------------Ejercio 5-------------");
+            var ejem5 = proys.Average(x => x.Duracion);
+            Console.WriteLine($"El promedio de duracion de todos los proyectos es:{ejem5}");
+            Console.ReadKey();
+            Console.WriteLine($"-----------------------------------" + "\n" + "-------------Ejercio 6-------------");
+            //var ejem6 = proys.Where(x => x.Codigo>0).Select(x=> x.Nombre && x.Duracion);
+            var ejem61 = proys.Max(x => x.Duracion);
+            var ejem6 = from p in proys
+                        where p.Duracion==ejem61
+                        select p.Nombre;
+            foreach (var item in ejem6)
+            {
+                Console.WriteLine($"El nombre de la maxima duracion es {item}");
+
+            }
+            Console.ReadKey();
+            Console.WriteLine($"-----------------------------------" + "\n" + "-------------Ejercio 7-------------");
+            var ejem7 = proys.Where(x => x.Duracion > 6 ).ToList();
+            foreach (var item in ejem7)
+            {
+                Console.WriteLine($"Nombre:  {item.Nombre}\nArea: {item.Area}");
+            }
+            Console.ReadKey();
+            Console.WriteLine($"-----------------------------------" + "\n" + "-------------Ejercio 8-------------");
+        }
+
+        public static double CalcularRaiz(double bas,double exp)
+        {
+            return Math.Pow(bas, exp);
+        }
+        
+    }
+}
---
+++ Proyecto.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dia7Delegados
+{
+    class Proyecto
+    {
+        public List<Proyecto> proyectos = new List<Proyecto>();
+        public string Nombre { get; set; }
+        public double Codigo { get; set; }
+        public string Area { get; set; }
+        public int Duracion { get; set; }
+        public List<Proyecto> AgregarProyecto() {
+            Console.WriteLine("Escriba y para ingresar");
+            var res = Console.ReadLine();
+            while (res=="y")
+            {
+                Console.WriteLine("Escriba el nombre del proyecto");
+                var nom = Console.ReadLine();
+                Console.WriteLine("Escriba código del proyecto");
+                var cod = Console.ReadLine();
+                var miProyecto = new Proyecto()
+                {
+                    Nombre = nom,
+                    Codigo = int.Parse(cod)
+                };
+                proyectos.Add(miProyecto);
+                Console.WriteLine("Escriba y para ingresar");
+                res = Console.ReadLine();
+            }
+            return proyectos;
+        }
+        public List<Proyecto> AgregarProyecto2()
+        {
+            var proys = new List<Proyecto>() {
+                new Proyecto()
+            {
+                Nombre = "Bancaria",
+                Codigo = 3,
+                Area="Comercio",
+                Duracion=12
+            },
+                new Proyecto()
+            {
+                Nombre = "Mercadeo",
+                Codigo = 1,
+                Area="Talento Humano",
+                Duracion=8
+            },
+                 new Proyecto()
+            {
+                Nombre = "Atencion Primera",
+                Codigo = 19,
+                Area="AIPI",
+                Duracion=20
+            },
+                 new Proyecto()
+            {
+                Nombre = "Banco",
+                Codigo = 14,
+                Area="Contabilidad",
+                Duracion=11
+            },
+                new Proyecto()
+            {
+                Nombre = "Mercadeo Supermercado",
+                Codigo = 40,
+                Area="Talento defensa",
+                Duracion=6
+            },
+                 new Proyecto()
+            {
+                Nombre = "Atencion Infantil",
+                Codigo = 8,
+                Area="ASP",
+                Duracion=88
+            }
+        };
+            return proys;
+            
+        }
+        //public List<Proyecto> AgregarProyecto3()
+        //{
+        //    var proys = new List<Proyecto>() {
+        //        new Proyecto()
+        //    {
+        //        Nombre = "Bancaria",
+        //        Codigo = 3,
+        //        Area="Comercio",
+        //        Duracion=12
+        //    },
+        //        new Proyecto()
+        //    {
+        //        Nombre = "Mercadeo",
+        //        Codigo = 23,
+        //        Area="Talento Humano",
+        //        Duracion=8
+        //    },
+        //         new Proyecto()
+        //    {
+        //        Nombre = "Atencion Primera",
+        //        Codigo = 19,
+        //        Area="AIPI",
+        //        Duracion=18
+        //    },
+        //         new Proyecto()
+        //    {
+        //        Nombre = "Banco",
+        //        Codigo = 1,
+        //        Area="Contabilidad",
+        //        Duracion=12
+        //    },
+        //        new Proyecto()
+        //    {
+        //        Nombre = "Mercadeo Supermercado",
+        //        Codigo = 40,
+        //        Area="Talento defensa",
+        //        Duracion=6
+        //    },
+        //         new Proyecto()
+        //    {
+        //        Nombre = "Atencion Infantil",
+        //        Codigo = 8,
+        //        Area="ASP",
+        //        Duracion=18
+        //    }
+        //};
+        //    return proys;
+
+        //}
+        public void Imprimir() {
+            proyectos = AgregarProyecto2();
+            Console.WriteLine($"Esta es la lista:");
+            foreach (var item in proyectos)
+            {
+
+                if (item.Codigo >= 20)
+                {
+                    Console.WriteLine($"Nombre: {item.Nombre}");
+                    Console.WriteLine($"Codigo: {item.Codigo}");
+                    Console.WriteLine($"Area: {item.Area}");
+                    Console.WriteLine($"Duracion: {item.Duracion}");
+                }
+                Console.WriteLine($"Esta es la lista: {item.Nombre}{item.Codigo}{item.Area}{item.Duracion}");
+            }
+        }
+        public void Imprimir2(List <Proyecto> proyectos) {
+            Console.WriteLine($"Esta es la Lista");
+
+            foreach (var item in proyectos)
+            {
+                Console.WriteLine($"Nombre: {item.Nombre}");
+                Console.WriteLine($"Codigo: {item.Codigo}");
+                Console.WriteLine($"Area: {item.Area}");
+                Console.WriteLine($"Duracion: {item.Duracion}");
+            }
+
+        }
+        //public void ImprimirProyect(List<Proyecto> List)
+        //{
+        //    foreach (var item in proyectos)
+        //    {
+        //        Console.WriteLine($"Nombre: {item.Nombre}");
+        //        Console.WriteLine($"Codigo: {item.Codigo}");
+        //        Console.WriteLine($"Area: {item.Area}");
+        //        Console.WriteLine($"Duracion: {item.Duracion}");
+        //    }
+        //}
+    }
+}
